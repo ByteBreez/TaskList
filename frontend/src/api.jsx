323706ccf6ec99b5c0417740b6ce0e9510f3c475
Abhishek Kumar Singh <jai123shriganesh@gmail.com>
2025-05-14 00:23:55 +0530

    debugging

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 // src/api.js
 import axios from 'axios';
-
+console.log('API baseURL:', import.meta.env.VITE_API_URL); 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,  // your Render backend
   withCredentials: true,                   // always send/receive cookies
